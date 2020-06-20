0dbe1f5148820c472a47a15db071d5a4c9df251f
PaperTurtlesStudio <66807368+PaperTurtlesStudio@users.noreply.github.com>
2020-06-19 21:51:58 -0700

    Day Whatever, did the stuff

---
+++ GabeGame3DRPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform model;
+
+    public float speed = 3f;
+    public CharacterController controller;
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    public float jumpHeight = 3f;
+    bool canJump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded)
+        {
+            canJump = true;
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        model.position = gameObject.transform.position;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+
+        if (x > 0 || z != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (animator.GetBool("isWalking") && Input.GetButton("Run"))
+        {
+            animator.SetBool("isRunning", true);
+            speed = 6f;
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+            speed = 3f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ GabeGame3DRPG/Assets/Scripts/mouseLook.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public float mouseSensivity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    bool thirdPerson = true;
+
+    Vector3 cameraFirstPerson = new Vector3(-0.033f, 0.835f, 0.096f);
+    Vector3 cameraThirdPerson = new Vector3(0.01f, 0.974f, -1.253f);
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        if(mouseY < 0.1 && mouseY > -0.1)
+        {
+            mouseY = 0;
+        }
+        if(mouseX < 0.1 && mouseX > -0.1)
+        {
+            mouseX = 0;
+        }
+
+        if (Input.GetButtonUp("Camera Switch"))
+        {
+            if (thirdPerson)
+            {
+                gameObject.transform.localPosition = cameraFirstPerson;
+                thirdPerson = false;
+            }
+            else
+            {
+                transform.localPosition = cameraThirdPerson;
+                thirdPerson = true;
+            }
+        }
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+
+        
+        
+
+        
+
+        
+
+
+
+
+    }
+}
